5a11b56cc0b692e2d708d5b27bc908769ae8e9db
BusUser <BusUser@EL1-MOB-WFS-28.home>
2020-04-06 09:58:41 -0400

    Initial Commit fresh code

---
+++ ProjectOne/src/com/nTier/HelloGIT.java
@@ -0,0 +1,10 @@
+package com.nTier;
+
+public class HelloGIT {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GIT World");
+
+	}
+
+}
---
+++ ProjectOne/src/com/nTier/SampleClass.java
@@ -0,0 +1,9 @@
+package com.nTier;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GIT World!");
+	}
+
+}
